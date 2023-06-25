8b345a3bde4721c32f5e31124de25b7f9b89a76d
HaradaMaria <132239927+HaradaMaria@users.noreply.github.com>
2023-06-25 02:08:22 +0000

    Finish mission7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,11 +4,11 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length: { maximum: 200 }
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search,ward)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{ward}")
@@ -22,4 +22,9 @@ class Book < ApplicationRecord
       @book = Book.all
     end
   end
+
+  scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
+  scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
+  scope :created_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
+  scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
 end
